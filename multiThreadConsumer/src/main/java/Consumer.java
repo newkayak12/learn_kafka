eb72e9f07a30d72d13c18e2fdd3aee05e79b3d0d
newkayak12 <newkayak12@gmail.com>
2024-03-29 21:47:44 +0900

    kafkaDetail

---
+++ multiThreadConsumer/src/main/java/Consumer.java
@@ -0,0 +1,48 @@
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.TopicPartition;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.Properties;
+import java.util.Set;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+
+public class Consumer {
+    private final static Logger logger = LoggerFactory.getLogger(Consumer.class);
+    private final static String TOPIC_NAME = "test";
+    private final static String BOOTSTRAP_SERVER = "192.168.0.11:9092";
+    private final static String GROUP_ID = "test-group";
+
+    public static void main(String[] args) {
+        Properties config = new Properties();
+        config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVER);
+        config.put(ConsumerConfig.GROUP_ID_CONFIG, GROUP_ID);
+        config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<String, String>(config);
+        consumer.subscribe(Arrays.asList(TOPIC_NAME));
+
+        ExecutorService executorService = Executors.newCachedThreadPool();
+
+        Set<TopicPartition> assignedTopicPartition = consumer.assignment();
+        while ( true ) {
+            ConsumerRecords<String , String > records = consumer.poll(Duration.ofSeconds(10));
+
+            for ( ConsumerRecord<String, String> record: records) {
+                ConsumerWorker worker = new ConsumerWorker(record.value());
+                executorService.execute(worker);
+            }
+        }
+
+
+    }
+}
---
+++ multiThreadConsumer/src/main/java/ConsumerWorker.java
@@ -0,0 +1,18 @@
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ConsumerWorker implements Runnable {
+
+    private final static Logger logger = LoggerFactory.getLogger(ConsumerWorker.class);
+    private String recordValue;
+
+    public ConsumerWorker(String recordValue) {
+        this.recordValue = recordValue;
+    }
+
+    @Override
+    public void run() {
+        logger.info("thread {}\trecord {}", Thread.currentThread().getName(), recordValue);
+    }
+}
