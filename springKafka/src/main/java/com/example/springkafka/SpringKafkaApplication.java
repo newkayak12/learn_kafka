e3b686819de008c7be2cae1d22b33912faca467f
newkayak12 <newkayak12@gmail.com>
2024-04-03 15:55:11 +0900

    kafka

--- springKafka/src/main/java/com/example/springkafka/SpringKafkaApplication.java
+++ springKafka/src/main/java/com/example/springkafka/SpringKafkaApplication.java
@@ -1,13 +1,59 @@
 package com.example.springkafka;
 
+import org.apache.kafka.clients.consumer.Consumer;
+import org.apache.kafka.common.TopicPartition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.listener.ConsumerAwareRebalanceListener;
+
+import java.util.Collection;
 
 @SpringBootApplication
 public class SpringKafkaApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringKafkaApplication.class, args);
+        ConcurrentKafkaListenerContainerFactory<String, String> factory = new ConcurrentKafkaListenerContainerFactory<>();
+
+        factory.getContainerProperties().setConsumerRebalanceListener( new ConsumerAwareRebalanceListener() {
+            @Override
+            public void onPartitionsRevokedBeforeCommit(Consumer<?, ?> consumer, Collection<TopicPartition> partitions) {
+                ConsumerAwareRebalanceListener.super.onPartitionsRevokedBeforeCommit(consumer, partitions);
+            }
+
+            @Override
+            public void onPartitionsRevokedAfterCommit(Consumer<?, ?> consumer, Collection<TopicPartition> partitions) {
+                ConsumerAwareRebalanceListener.super.onPartitionsRevokedAfterCommit(consumer, partitions);
+            }
+
+            @Override
+            public void onPartitionsLost(Consumer<?, ?> consumer, Collection<TopicPartition> partitions) {
+                ConsumerAwareRebalanceListener.super.onPartitionsLost(consumer, partitions);
+            }
+
+            @Override
+            public void onPartitionsAssigned(Consumer<?, ?> consumer, Collection<TopicPartition> partitions) {
+                ConsumerAwareRebalanceListener.super.onPartitionsAssigned(consumer, partitions);
+            }
+
+            @Override
+            public void onPartitionsRevoked(Collection<TopicPartition> partitions) {
+                ConsumerAwareRebalanceListener.super.onPartitionsRevoked(partitions);
+            }
+
+            @Override
+            public void onPartitionsAssigned(Collection<TopicPartition> partitions) {
+                ConsumerAwareRebalanceListener.super.onPartitionsAssigned(partitions);
+            }
+
+            @Override
+            public void onPartitionsLost(Collection<TopicPartition> partitions) {
+                ConsumerAwareRebalanceListener.super.onPartitionsLost(partitions);
+            }
+        });
+
+
     }
 
 }
